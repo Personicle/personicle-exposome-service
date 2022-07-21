f06e045ace4843a7c3a12d02b047ce2ebda6d970
Vaibhav Pandey <vpandey@clearsense.com>
2022-07-21 13:06:21 -0700

    Added random shuffling through api keys

--- exposomeFunction/__init__.py
+++ exposomeFunction/__init__.py
@@ -19,6 +19,8 @@ from geopy.geocoders import Nominatim
 import traceback
 import azure.functions as func
 
+from random import choice, shuffle
+
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.utcnow()
@@ -30,132 +32,138 @@ def main(mytimer: func.TimerRequest) -> None:
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
-    api_key_weather = "093c33903fd3e82d277fcf59a4f550ac"
+    API_KEYS = os.environ.get("OPENWEATHER_KEYS").split(":")
+    shuffle(API_KEYS)
+    logging.info(API_KEYS)
+    for api_key in API_KEYS:
+        # api_key = choice(API_KEYS)
 
-    try:
-        query_events = 'select * from  users '
-        df_users = sqlio.read_sql_query(query_events, engine)
-        temp_info = pd.DataFrame(
-            df_users['info'].values.tolist(), index=df_users.index)
-        ud = df_users.copy()
+        try:
+            query_events = 'select * from  users '
+            df_users = sqlio.read_sql_query(query_events, engine)
+            temp_info = pd.DataFrame(
+                df_users['info'].values.tolist(), index=df_users.index)
+            ud = df_users.copy()
 
-        geolocator = Nominatim(user_agent="geoapiExercises")
-        temp_info['latitude'] = (temp_info['zipcode']+' '+temp_info['country']
-                                 ).swifter.apply(lambda x: geolocator.geocode(x).latitude)
-        temp_info['longitude'] = (temp_info['zipcode']+' '+temp_info['country']
-                                  ).swifter.apply(lambda x: geolocator.geocode(x).longitude)
+            geolocator = Nominatim(user_agent="geoapiExercises")
+            temp_info['latitude'] = (temp_info['zipcode']+' '+temp_info['country']
+                                     ).swifter.apply(lambda x: geolocator.geocode(x).latitude)
+            temp_info['longitude'] = (temp_info['zipcode']+' '+temp_info['country']
+                                      ).swifter.apply(lambda x: geolocator.geocode(x).longitude)
 
-        ud_final = pd.concat([ud[['user_id']], temp_info[[
-            'country', 'zipcode', 'latitude', 'longitude']]], axis=1)  # .to_csv('test.csv')
+            ud_final = pd.concat([ud[['user_id']], temp_info[[
+                'country', 'zipcode', 'latitude', 'longitude']]], axis=1)  # .to_csv('test.csv')
 
-        for i in range(ud_final.shape[0]):
-            if ud_final['zipcode'].iloc[i] is None:
-                continue
+            for i in range(ud_final.shape[0]):
+                if ud_final['zipcode'].iloc[i] is None:
+                    continue
 
-            lat = np.float(ud_final['latitude'].iloc[i])
-            lon = np.float(ud_final['longitude'].iloc[i])
+                lat = np.float(ud_final['latitude'].iloc[i])
+                lon = np.float(ud_final['longitude'].iloc[i])
 
-            api_key_weather = "8b4545eed9dc9ba39fcf91ff79d26930"
-            weather_url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (
-                lat, lon, api_key_weather)
+                weather_url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (
+                    lat, lon, api_key)
 
-            response = requests.get(weather_url)
-            data = json.loads(response.text)
+                response = requests.get(weather_url)
+                data = json.loads(response.text)
 
-            logging.info("Weather api response")
-            logging.info(data)
+                logging.info("Weather api response")
+                logging.info(data)
 
-            df = pd.json_normalize(data)
-            df.columns = [col.replace('current.', '') for col in df.columns]
+                df = pd.json_normalize(data)
+                df.columns = [col.replace('current.', '')
+                              for col in df.columns]
 
-            df_weather = df[['dt', 'temp', 'feels_like',
-                            'pressure', 'humidity', 'uvi']].copy()
-            df_weather = df_weather.melt(id_vars='dt').copy()
+                df_weather = df[['dt', 'temp', 'feels_like',
+                                'pressure', 'humidity', 'uvi']].copy()
+                df_weather = df_weather.melt(id_vars='dt').copy()
 
-            dct_weather = {'temp': 'C', 'feels_like': 'C',
-                           'pressure': 'hPa', 'humidity': '%', 'uvi': 'mwpscc'}
+                dct_weather = {'temp': 'C', 'feels_like': 'C',
+                               'pressure': 'hPa', 'humidity': '%', 'uvi': 'mwpscc'}
 
-            df_weather['unit'] = df_weather['variable'].map(dct_weather)
+                df_weather['unit'] = df_weather['variable'].map(dct_weather)
 
-            api_key_pol = "093c33903fd3e82d277fcf59a4f550ac"
+                pollution_url = "http://api.openweathermap.org/data/2.5/air_pollution?lat=%s&lon=%s&appid=%s" % (
+                    lat, lon, api_key)
+                response1 = requests.get(pollution_url)
+                data1 = json.loads(response1.text)
 
-            pollution_url = "http://api.openweathermap.org/data/2.5/air_pollution?lat=%s&lon=%s&appid=%s" % (
-                lat, lon, api_key_pol)
-            response1 = requests.get(pollution_url)
-            data1 = json.loads(response1.text)
+                logging.info("Pollution api response")
+                logging.info(data1)
 
-            logging.info("Pollution api response")
-            logging.info(data1)
+                df_pol = pd.json_normalize(data1)
 
-            df_pol = pd.json_normalize(data1)
+                df_pol = pd.DataFrame.from_dict(df_pol.list.values[0][0:])
 
-            df_pol = pd.DataFrame.from_dict(df_pol.list.values[0][0:])
+                json_struct = json.loads(df_pol.to_json(orient="records"))
+                df_pol = pd.io.json.json_normalize(
+                    json_struct, errors='ignore').copy()
 
-            json_struct = json.loads(df_pol.to_json(orient="records"))
-            df_pol = pd.io.json.json_normalize(
-                json_struct, errors='ignore').copy()
+                df_pol.columns = df_pol.columns.str.replace('main.', '')
+                df_pol.columns = df_pol.columns.str.replace('components.', '')
 
-            df_pol.columns = df_pol.columns.str.replace('main.', '')
-            df_pol.columns = df_pol.columns.str.replace('components.', '')
+                df_pol = df_pol.melt(id_vars='dt').copy()
 
-            df_pol = df_pol.melt(id_vars='dt').copy()
+                df_pol['unit'] = 'ug/cc'
+                df_comb = pd.concat([df_pol, df_weather], ignore_index=True)
+                df_comb['individual_id'] = ud_final['user_id'].iloc[i]
+                df_comb['source'] = 'org.personicle.exposome'
+                df_comb.rename(columns={'variable': 'streamName',
+                                        'dt': 'timestamp'}, inplace=True)
 
-            df_pol['unit'] = 'ug/cc'
-            df_comb = pd.concat([df_pol, df_weather], ignore_index=True)
-            df_comb['individual_id'] = ud_final['user_id'].iloc[i]
-            df_comb['source'] = 'org.personicle.exposome'
-            df_comb.rename(columns={'variable': 'streamName',
-                                    'dt': 'timestamp'}, inplace=True)
+                df_comb['streamName'] = 'com.personicle.individual.datastreams.exposome.' + \
+                    df_comb['streamName']
 
-            df_comb['streamName'] = 'com.personicle.individual.datastreams.exposome.' + \
-                df_comb['streamName']
+                df_comb = df_comb[['streamName', 'individual_id',
+                                   'source', 'unit', 'timestamp', 'value']].copy()
 
-            df_comb = df_comb[['streamName', 'individual_id',
-                               'source', 'unit', 'timestamp', 'value']].copy()
+                df_comb['timestamp'] = pd.to_datetime(
+                    df_comb['timestamp'], unit='s')
 
-            df_comb['timestamp'] = pd.to_datetime(
-                df_comb['timestamp'], unit='s')
+                df_comb['timestamp'] = df_comb['timestamp'].astype(str)
 
-            df_comb['timestamp'] = df_comb['timestamp'].astype(str)
+                logging.info("logging weather data packet")
+                logging.info(df_comb)
 
-            logging.info("logging weather data packet")
-            logging.info(df_comb)
+                l1 = df_comb.to_dict(orient='records')
 
-            l1 = df_comb.to_dict(orient='records')
+                data = l1.copy()
 
-            data = l1.copy()
+                res = []
 
-            res = []
+                def key_func(k): return (k['streamName'],
+                                         k['individual_id'], k['unit'], k['source'])
 
-            def key_func(k): return (k['streamName'],
-                                     k['individual_id'], k['unit'], k['source'])
+                for k, g in groupby(sorted(data, key=key_func), key=key_func):
+                    obj = {'streamName': k[0], 'individual_id': k[1],
+                           'source': k[3], 'unit': k[2], 'dataPoints': []}
+                    for group in g:
+                        d = {}
 
-            for k, g in groupby(sorted(data, key=key_func), key=key_func):
-                obj = {'streamName': k[0], 'individual_id': k[1],
-                       'source': k[3], 'unit': k[2], 'dataPoints': []}
-                for group in g:
-                    d = {}
+                        if ('timestamp' in group):
+                            d['timestamp'] = group['timestamp']
 
-                    if ('timestamp' in group):
-                        d['timestamp'] = group['timestamp']
+                        if ('value' in group):
+                            d['value'] = group['value']
 
-                    if ('value' in group):
-                        d['value'] = group['value']
+                        obj['dataPoints'].append(d)
+                    res.append(obj)
 
-                    obj['dataPoints'].append(d)
-                res.append(obj)
+                try:
+                    for i in range(len(res)):
+                        datastream_producer(res[i])
 
-            try:
-                for i in range(len(res)):
-                    datastream_producer(res[i])
+                except Exception as e:
 
-            except Exception as e:
-
-                logging.info("Total data points added for source {}: {}".format(
-                    datastream_producer(res[i]), len(datastream_producer(res[i]))))
-                logging.error(traceback.format_exc())
-        logging.info({"msg": "successful request at {}".format(utc_timestamp)})
-    except Exception as e:
-        logging.error({"msg": "unsuccessful request at {}".format(utc_timestamp),
-                       "error": e})
-        logging.error(traceback.format_exc())
+                    logging.info("Total data points added for source {}: {}".format(
+                        datastream_producer(res[i]), len(datastream_producer(res[i]))))
+                    logging.error(traceback.format_exc())
+            logging.info(
+                {"msg": "successful request at {}".format(utc_timestamp)})
+            break
+        except Exception as e:
+            logging.error({"msg": "unsuccessful request at {} for api key: {}".format(utc_timestamp, api_key),
+                           "error": e})
+            logging.error(traceback.format_exc())
+            continue
+    return
